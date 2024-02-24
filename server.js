c900381864d2182e6f74ced1ef203199d06460a6
Silvia Chen <silviachen@Silvia-Chens-MacBook-Pro-1292.local>
2024-02-24 02:18:09 -0800

    server side setup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const Song = require("./models/song.js")
 const app = express()
 
 mongoose.connect("mongodb://localhost/songRecorder")
@@ -12,8 +13,26 @@ app.get("/", (req, res) => {
   res.render("index")
 })
 
-app.post("/songs", (req, res) => {
-  req.body.songNotes
+// save to database
+app.post("/songs", async (req, res) => {
+  const song = new Song({
+    notes: req.body.songNotes,
+  })
+
+  await song.save()
+
+  // in order to get the id of the song, so we can use it later
+  res.json(song)
+})
+
+app.get("/songs/:id", async (req, res) => {
+  let song
+  try {
+    song = await Song.findById(req.params.id)
+  } catch (e) {
+    song = undefined
+  }
+  res.render("index", { song: song })
 })
 
 app.listen(5001)
